697ab47648a6b4bdf875c77901b2f8ed3650aad9
Dennis-Farias <dennisfarias3@gmail.com>
2023-04-18 18:00:25 +0000

    Primeiro Servlet

---
+++ app/src/main/java/exservlets/PrimeiroServlet.java
@@ -0,0 +1,16 @@
+package exservlets;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "IndexServlet", urlPatterns = "/")
+public class PrimeiroServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+    throws ServletException, IOException {
+        response.getWriter().print("Olá Servlet!!!");
+    } 
+}
